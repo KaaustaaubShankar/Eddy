c1248c2d811078077e2e780ae48b7caf71a2ccee
KaaustaaubShankar <76492384+KaaustaaubShankar@users.noreply.github.com>
2024-08-24 11:10:28 -0400

    Initial Commit

---
+++ Eddy/EddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EddyApp.swift
+//  Eddy
+//
+//  Created by Kaaustaaub Shankar on 8/24/24.
+//
+
+import SwiftUI
+
+@main
+struct EddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
